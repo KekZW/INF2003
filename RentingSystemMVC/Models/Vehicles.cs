76605833f879f3c11919a875c93f3fe4caab6566
KekZW <147482043+KekZW@users.noreply.github.com>
2024-05-28 09:55:15 +0800

    Add files via upload

---
+++ RentingSystemMVC/Models/Vehicles.cs
@@ -0,0 +1,15 @@
+﻿namespace RentingSystemMVC.Models
+{
+    public class Vehicles
+    {
+        public int VehicleID { get; set; }
+        public string VehicleName { get; set; }
+        public string Description { get; set; }
+        public string LicensePlate { get; set; }
+        public string FuelType { get; set; }
+        public float FuelTankCapacity { get; set; }
+        public float FuelLevel { get; set; }
+        public string LicenseToOperate { get; set; }
+    }
+}
+
