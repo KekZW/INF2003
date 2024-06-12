22216cdf09152608e18373cfb8ccbd089e928204
KekZW <kekzhiwei@outlook.com>
2024-06-12 18:59:57 +0800

    Update, allow user to view rental

---
+++ RentingSystemMVC/Models/Rental.cs
@@ -0,0 +1,15 @@
+﻿namespace RentingSystemMVC.Models
+{
+    public class Rental
+    {
+        public int rentalID { get; set; }
+        public int UserID { get; set; }
+        public int vehicleID { get; set; }
+        public string LicensePlate { get; set; }
+        public DateTime startRentalDate { get; set; }
+        public DateTime endRentalDate { get; set; }
+        public decimal rentalAmount { get; set; }
+        public string rentalAddress { get; set; }
+        public int rentalLot { get; set; }
+    }
+}
