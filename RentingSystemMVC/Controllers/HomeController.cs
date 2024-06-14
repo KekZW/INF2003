4246867be3d18c8c758c922a26439a5e95fe2a83
Edwin <hewzhongxuan6f@gmail.com>
2024-06-15 03:14:51 +0800

    Used sql to display the top few cars in the carousel main page, currently manage vehicles have error due to the fact that we use timesrented in our VehicleViewModel which disrupted the other sql statements. A possible solution is to create another vehicle model that does not contain TimesRented

--- RentingSystemMVC/Controllers/HomeController.cs
+++ RentingSystemMVC/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-using Microsoft.AspNetCore.Mvc;
-using RentingSystemMVC.Models;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Claims;
+using System.Windows.Input;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using RentingSystemMVC.Data;
+using RentingSystemMVC.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace RentingSystemMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string _connectionString = "Server=localhost;Database=vehicleDB;Uid=root;Pwd=;";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,7 +24,52 @@ namespace RentingSystemMVC.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            
+            
+            List<VehicleViewModel> vehicles = new List<VehicleViewModel>();
+            
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                string query = 
+                    "SELECT v.vehicleID, v.licensePlate, v.licenseToOperate, vt.brand, vt.model, vt.type, " +
+                    "vt.seats, vt.fuelCapacity, vt.fuelType, vt.truckSpace, vt.rentalCostPerDay, COUNT(r.vehicleID) AS timesRented " +
+                    "FROM vehicle v " +
+                    "INNER JOIN vehicleType vt ON v.vehicleTypeID = vt.vehicleTypeID " +
+                    "LEFT JOIN rental r ON v.vehicleID = r.vehicleID " +
+                    "GROUP BY v.vehicleID " +
+                    "ORDER BY timesRented DESC " +
+                    "LIMIT 6";
+
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            VehicleViewModel vehicle = new VehicleViewModel()
+                            {
+                                VehicleID = reader.GetInt32("vehicleID"),
+                                LicensePlate = reader.GetString("licensePlate"),
+                                LicenseToOperate = reader.GetString("licenseToOperate"),
+                                Brand = reader.GetString("brand"),
+                                Model = reader.GetString("model"),
+                                Type = reader.GetString("type"),
+                                Seats = reader.GetInt32("seats"),
+                                FuelCapacity = reader.GetDecimal("fuelCapacity"),
+                                FuelType = reader.GetString("fuelType"),
+                                TruckSpace = reader.GetDecimal("truckSpace"),
+                                RentalCostPerDay = reader.GetDecimal("rentalCostPerDay"),
+                                TimesRented = reader.GetInt32("timesRented")
+                            };
+                            vehicles.Add(vehicle);
+
+                        }
+                    }
+                }
+            }
+            
+            return View(vehicles);
         }
 
         public IActionResult Privacy()
