8c62948bb2c7886738f4fe1d38ce28770e6719a5
Isaiah-Ang <isaiah.ang@gmail.com>
2024-06-15 11:09:26 +0800

    Edit user roles

--- RentingSystemMVC/Models/User.cs
+++ RentingSystemMVC/Models/User.cs
@@ -3,11 +3,11 @@ namespace RentingSystemMVC.Models;
 public class User
 {
     public int UserID { get; set; }
-    public string Username { get; set; }
     public string UserPassword { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
     public int LicenseID { get; set; }
     public string EmailAddress { get; set; }
     public string PhoneNo { get; set; }
+    public string Role { get; set; }
 }
