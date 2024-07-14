20c2537955c5a3dc9e7718e70f45ff2169a100c1
Virgil Dsouza <147490176+virgil-SD@users.noreply.github.com>
2024-07-15 00:04:39 +0800

    Updated maintenance

--- RentingSystemMVC/Models/Maintenance.cs
+++ RentingSystemMVC/Models/Maintenance.cs
@@ -1,17 +1,39 @@
 using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace RentingSystemMVC.Models
 {
     public class Maintenance
     {
-        [Key]
+        [BsonElement("MaintenanceID")]
         public int MaintenanceID { get; set; }
+
         public int VehicleID { get; set; }
+
+        [BsonElement("WorkshopStatus")]
         public string WorkshopStatus { get; set; }
+
+        [BsonElement("startMaintDate")]
         public DateOnly startMaintDate { get; set; }
+
+        [BsonElement("endMaintDate")]
         public DateOnly endMaintDate { get; set; }
         
         // Navigation Property
         public Vehicle Vehicle { get; set; }
+    }
+
+    public class MaintenanceRecords
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; } = null;
+
+        [BsonElement("vehicleID")]
+        public int vehicleID { get; set; }
+
+        [BsonElement("maintenanceRecords")]
+        public List<Maintenance> maintenanceRecords { get; set; } = new List<Maintenance>();
     }
 }
--- RentingSystemMVC/Models/VehicleDetailModel.cs
+++ RentingSystemMVC/Models/VehicleDetailModel.cs
@@ -4,5 +4,6 @@ public class VehicleDetailModel: VehicleViewModel
 {
     public VehicleViewModel Vehicle { get; set; }
     public List<Maintenance>? Maintenances { get; set; }
+    public MaintenanceRecords? maintenanceRecords { get; set; }
     public VehicleReview? vehicleReview { get; set; }
 }
